1b6fff10944830aa1c43e2718b5d237c745776e0
patelaaman <amanpatel51251@gmail.com>
2025-06-09 16:55:03 +0530

    adkd

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function alertCTA() {
   alert("Thank you for showing interest! We'll get back to you soon.");
 }
+
+ function toggleFaq(el) {
+      const icon = el.querySelector('i');
+      const isOpen = el.classList.toggle('open');
+      const answer = el.querySelector('.faq-answer');
+      
+      icon.classList.toggle('fa-chevron-down', !isOpen);
+      icon.classList.toggle('fa-chevron-up', isOpen);
+      
+      if (isOpen) {
+        answer.style.display = 'block';
+      } else {
+        answer.style.display = 'none';
+      }
+    }
